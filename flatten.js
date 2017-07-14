fb6193539213c7d6578eb82f8afeedf42953c3ff
Jeremy Schrader <schraj@gmail.com>
2017-07-14 10:57:52 -0700

    add tests

--- flatten.js
+++ flatten.js
@@ -1,23 +1,54 @@
 // flatten an array of arbitrarily nested arrays of integers into a flat array of integers
-module.exports = arr => {
+flatten = arr => {
+    let _hasNonIntegerItems = false;
+
     // validate that the input is an array
     if (!(arr instanceof Array)) {
         return undefined;
     }
 
-    // validate that each item in the array is an integer
-    arr.forEach(i => {
-
-    });
-
-    // The reduce() method applies a function against an accumulator and each element in the array (from left to right)
+    // The reduce() method applies a function to an accumulator and each element in the array (from left to right)
     // to reduce it to a single value.  In this case, it will just keep appending either:
     // 1) a non array integer
     // 2) the result of the recursive call into itself
     // onto the existing array.
 
     // In the case that the current item(b) is an array, the function(flatten) will call itself recursively
-    return arr.reduce((a, b) => {
-        a.concat(Array.isArray(b) ? flatten(b) : b)
+    let flattenedArray = arr.reduce((a, b) => {
+        let currentItem = b;        
+        if (Array.isArray(currentItem)) {
+           currentItem = flatten(currentItem);
+
+            // validate that each item in this current array that we are concatenating to the 
+            // accumulator is an 
+            currentItem.forEach(i => {
+               _hasNonIntegerItems = _hasNonIntegerItems || isNonInteger(i);
+            });
+        } else {
+            _hasNonIntegerItems = _hasNonIntegerItems || isNonInteger(currentItem);
+        }
+
+        // current item here is either a single non array integer
+        // or an array of integers as created by a recursive call to this function
+        return a.concat(currentItem);
     }, []);    
+    
+    if (_hasNonIntegerItems) {
+        throw new Error('Array has non-numeric items');
+    }
+
+    return flattenedArray;
 }
+
+// Check whether value is not an integer
+isNonInteger = val => {
+    // If isNaN(x) returns false, you can use x in an arithmetic expression not making the expression return NaN. 
+    // If it returns true, x will make every arithmetic expression return NaN.
+    if (isNaN(val) || !Number.isInteger(val)) {
+        return true;
+    }       
+
+    return false;
+}
+
+module.exports = flatten;
--- test/flatten.test.js
+++ test/flatten.test.js
@@ -1,26 +1,55 @@
-// var list1 = [[0, 1], [2, 3], [4, 5]];
-// var list2 = [0, [1, [2, [3, [4, [5]]]]]];
-// const t1 = flatten(list1); 
-// // returns [0, 1, 2, 3, 4, 5]
-
-// const t2 = flatten(list2); 
-
 const flatten = require('../flatten');
-
 const assert = require('assert');
-describe('Flatten', function() {
-    it('should return undefined when the parameter is not provided', function() {
-        const result = flatten();
-        assert.equal(undefined, result);
+
+describe('Flatten', function () {
+    it('should return an empty array when provided one', function () {
+        const input = [];
+        const expected = 0;
+        const result = flatten(input);
+        assert.equal(result.length, 0);
     });
 
-    it('should return undefined when the parameter is not an array', function() {
-        const result = flatten(1);
-        assert.equal(undefined, result);
+    it('should return same array when it is not nested', function () {
+        const input = [1, 2];
+        const expected = [1, 2];
+        const result = flatten(input);
+        assert.deepEqual(result, expected);
     });
 
-    it('should return undefined when the parameter array has a non-integer value within it', function() {
-        const result = flatten([1,'a',2]);
-        assert.equal(undefined, result);
+    it('should return flattened array when nested at single level', function () {
+        const input = [[0, 1], [2, 3], [4, 5]];
+        const expected = [0, 1, 2, 3, 4, 5];
+        const result = flatten(input);
+        assert.deepEqual(result, expected);
+    });
+
+    it('should return flattened array when nested to multiple levels', function () {
+        const input = [0, [1, [2, [3, [4, [5]]]]]];
+        const expected = [0, 1, 2, 3, 4, 5];
+        const result = flatten(input);
+        assert.deepEqual(result, expected);
+    });
+
+    describe('When Validating Arguments', function () {
+        it('should return undefined when the parameter is not an array', function () {
+            const result = flatten(1);
+            assert.equal(undefined, result);
+        });
+
+        it('should throw an error when the parameter array has a non-integer/letter value within it', function () {
+            assert.throws(() => flatten([1, 'a', 2]));
+        });
+
+        it('should throw an error when the parameter array has a non-integer/float value within it', function () {
+            assert.throws(() => flatten([1, 'a', 2]));
+        });
+
+        it('should throw an error when the parameter array has a non-integer/object value within it', function () {
+            assert.throws(() => flatten([{}, 2]));
+        });
+
+        it('should throw an error when the parameter array has a non-integer value in a nested array within it', function () {
+            assert.throws(() => flatten([1, [2, 'a'], 4]));
+        });
     });
 });
